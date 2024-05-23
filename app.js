5f26ba2960bd345719bc62782c732ede28d82921
ubaiidraza <154611273+ubaiidraza@users.noreply.github.com>
2024-05-23 18:41:26 +0500

    input form link to js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const userName = document.querySelector('#userName')
+const email = document.querySelector('#email')
+const nicNumber = document.querySelector('#nicNumber')
+const password = document.querySelector('#password')
+const btn = document.querySelector('#btn')
+
